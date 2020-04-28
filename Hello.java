d28606859a20301fc123e59e6195b473d14e731d
dioStaxis <hd.ion333@gmail.com>
2020-04-29 10:07:07 +1200

    added hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args){
+        //Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Hello?");
+        //String userIn = scanner.nextLine();
+        //System.out.println("You answered: "+userIn);
+        //scanner.close();
+    }
+}

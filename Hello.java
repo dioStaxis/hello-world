0ebd65517f1a0c0692215fcb3f3c7bdec47ea97d
dioStaxis <hd.ion333@gmail.com>
2020-04-29 10:20:51 +1200

    removed ant deps

--- Hello.java
+++ Hello.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args){
-        //Scanner scanner = new Scanner(System.in);
-        
         System.out.println("Hello?");
-        //String userIn = scanner.nextLine();
-        //System.out.println("You answered: "+userIn);
-        //scanner.close();
     }
 }
